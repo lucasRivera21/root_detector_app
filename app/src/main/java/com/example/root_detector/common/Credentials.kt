29ddbbf1de44b6e0ba630f65ee60eabfbd31842b
lucas <lucasriverar2001@gmail.com>
2026-03-08 11:56:58 -0500

    REFACTOR: Update base url to production server in credentials.kt

--- app/src/main/java/com/example/root_detector/common/Credentials.kt
+++ app/src/main/java/com/example/root_detector/common/Credentials.kt
@@ -2,6 +2,6 @@ package com.example.root_detector.common
 
 // Use 10.0.2.2 for Android Emulator to access host localhost
 // Use your machine's local IP (e.g. 192.168.x.x) for physical device
-const val BASE_URL = "http://192.168.1.67:8000/"
+const val BASE_URL = "https://lucasrivera21-root-detector-v2.hf.space/"
 
 const val IMG_NAME_RESPONSE = "image_response.jpg"
